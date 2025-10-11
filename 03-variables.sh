e38139279843c9e1a6cd69b83da331abc2bac567
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-11 22:21:58 +0530

    some massage

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1 :: hello $PERSON2 how are you"
+echo "$PERSON2 :: yep hey hai $PERSON1 i am good, what about you"
+echo "$PERSON1:: i am also good $PERSON2 thanks for asking "
+echo "$PERSON2 :: how was your family "
+echo "$PERSON1 :: yeah all good $PERSON2"
+echo "$PERSON2 :: wath about your family $PERSON1 anuty and uncle are fine "
+echo "$PERSON1 :: yeah they are fine $PERSON2 thank you da "
