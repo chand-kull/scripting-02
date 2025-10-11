954feda1dfa0bc04bb8a60462cdbe68f1c054418
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-11 22:35:00 +0530

    some massage

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,10 +2,10 @@
 
 echo "place enter username"
 
-read -s "username"
+read -s username
 
 echo "place enter password"
 
-read -s "password"
+read -s password
 
 echo "username is $USERNAME: password is $PASSWORD"
