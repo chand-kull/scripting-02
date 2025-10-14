7bd0e8c7dfb0daded4c16f1ef48f7f0d16fbed09
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-14 23:50:19 +0530

    some massage

--- 08-installation.sh
+++ 08-installation.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-VALIDATE()
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo "$2....failure"
@@ -11,7 +11,7 @@ VALIDATE()
         echo "$2....success"
     if
 
-
+}
 if [ $USERID -ne 0]
 then
     echo "please run this script with root access."
