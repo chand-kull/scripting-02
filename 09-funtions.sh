4d4673589767f27a66992002c0e1b5282310c1e8
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-17 05:05:49 +0530

    hello this is chandu

--- 09-funtions.sh
+++ 09-funtions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-VALIDATE()
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo "$2.....FAILURE"
@@ -10,6 +10,7 @@ VALIDATE()
     else
         echo "$2......SUCCESS"
     if
+}
     
 
 if [ $USERID -ne 0 ]
