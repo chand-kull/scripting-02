ab6d4bca9965bdfdf3a78d2bd75e485e2453766f
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-17 04:38:59 +0530

    hello this is chandu

--- 09-funtions.sh
+++ 09-funtions.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Function to validate previous command
-VALIDATE() {
-    if [ $1 -ne 0 ]; then
-        echo "❌ ERROR: $2 failed."
-        exit 1
-    else
-        echo "✅ SUCCESS: $2 completed."
-    fi
-}
-
-# Check if user is root
 USERID=$(id -u)
-if [ "$USERID" -ne 0 ]; then
-    echo "⚠️  Please run this script with root access."
+
+if [ $USERID -ne 0 ]
+
+then
+    echo " please run this script with root access"
     exit 1
 else
-    echo "🔐 You are running as root (superuser)."
-fi
+    echo "you are super user "
+fi 
 
-# Install MySQL
-yum install mysql -y
-VALIDATE $? "Installing MySQL"
+
+yum install mysql -y 
+VALIDATE $? "installing MYSQL."
+
+yum install docker -y
+VALIDATE $? "installing docker"
