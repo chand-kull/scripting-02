8526cdb7f20b1c8f3b60c54d78459e5d5187b237
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-10-17 04:58:14 +0530

    hello this is chandu

--- 09-funtions.sh
+++ 09-funtions.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.....FAILURE"
+        exit 1
+    else
+        echo "$2......SUCCESS"
+    if
+}
+
+if [ $USERID -ne 0 ]
 then
     echo " please run this script with root access"
     exit 1
